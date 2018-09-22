492e28efb973503496bd90bfcd00e24a4eb189a8
Ming Yan <mingyan.vt@gmail.com>
2018-09-21 17:56:10 -0700

    Initial Commit

---
+++ dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dice
+//
+//  Created by Ming Yan on 9/21/18.
+//  Copyright © 2018 Ming Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
